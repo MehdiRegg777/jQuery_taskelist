f6b2f942a342eade4ae93f97b312d7d27673a2e7
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2024-01-15 21:17:59 +0100

    zz

--- www/js/index.js
+++ www/js/index.js
@@ -29,30 +29,114 @@ function onDeviceReady() {
     
     
 
-    $("#nuevaListaButton").on("click",añadirElemento);
-    $("#eliminarListaButton").on("click",eliminarElemento);
+
+    $("#nuevaListaButton").on("click", añadirElemento);
+/*     $("#editButton").on("click", editarElemento);
+    $("#deleteButton").on("click", eliminarElemento); */
+    $(document).on("click", ".deleteButton", eliminarElemento);
+    $(document).on("click", ".editButton", editarElemento);
 
 }
+function añadirElemento() {
+    var text = prompt("Añadir tasca:");
+    if (text !== null && text.trim() !== "") {
+        var $elem = $("<li><a href='#" + text + "'>" + text + "</a></li>");
+        var $deleteButton = $("<button class='delTask deleteButton' style='float: right;'>Delete</button>");
+        var $editButton = $("<button class='delTask editButton' style='float: right;'>Edit</button>");
 
-function añadirElemento(){
-    var texto = prompt("Añadir tasca: ");
-    var $elem = $("<li><a href=#"+texto.toLowerCase()+">" + texto + "</a></li>");
-    $("ul").append($elem);
-    //$("ul.li-listview").listview("refresh");
-    $('ul[data-role="listview"]').listview("refresh");
+        $elem.find('a').append($deleteButton);
+        $elem.find('a').append($editButton);
+
+        $("ul").append($elem);
+
+        var $page = $("<div data-role='page' id='" + text + "'></div>");
+        var $header = $("<div data-role='header'><a href='#' data-icon='back' data-rel='back' title='Go back'>Back</a><h1>" + text + "</h1></div>");
+        var $content = $("<div class='ui-content'><p>This is " + text + "</p></div>");
+        var $footer = $("<div data-role='footer' data-position='fixed'><h1>" + text + "</h1></div>");
+
+        $page.append($header);
+        $page.append($content);
+        $page.append($footer);
+
+        $("body").append($page);
+
+        $('ul[data-role="listview"]').listview('refresh');
+    }
 }
 
 function eliminarElemento() {
-    var texto = prompt("Eliminar tarea: ");
-    console.log(texto);
+    var $a = $(this).closest("a");
+    var taskID = $a.attr("href").replace("#", "");
+    var $li = $a.closest("li");
+    $li.remove();
+    $("#" + taskID).remove();
+    $('ul[data-role="listview"]').listview('refresh');
+}
 
-    // Seleccionar y eliminar el elemento específico que contiene el texto
-    $('li:contains(' + texto + ')').filter(function() {
-        return $(this).text().trim() === texto;
-    }).remove();
+function editarElemento() {
+    var $a = $(this).closest("a");
+    var currentText = $a.text();
+    var newText = prompt("Editar tasca:");
 
-    // Actualizar el listview
-    $('ul[data-role="listview"]').listview("refresh");
+    if (newText !== null && newText.trim() !== "") {
+        $a.text(newText);
+        var taskID = $a.attr("href").replace("#", "");
+        $("#" + taskID + " h1").text(newText);
+        $("#" + taskID + " p").text("This is " + newText);
+
+        $a.attr('href', '#' + newText);
+
+        var $deleteButton = $("<button class='delTask deleteButton' style='float: right;'>Delete "  + "</button>");
+        var $editButton = $("<button class='delTask editButton' style='float: right;'>Edit "  + "</button>");
+
+        $a.siblings('.deleteButton').remove();
+        $a.siblings('.editButton').remove();
+        $a.append($deleteButton);
+        $a.append($editButton);
+        console.log(currentText);
+        $("#ff").attr("id", newText);
+        $("#" + newText + " h1").text(newText);
+        $("#" + newText + " p").text("This is " + newText);
+        $("#" + newText + " .ui-content p").text("This is " + newText);
+        $("#" + newText + " .ui-footer h1").text(newText);
+    }
+    $('ul[data-role="listview"]').listview('refresh');
+    $('div[data-role="page"]').listview('refresh');
 }
 
 
+/* function edita(ev) {
+    console.log("edita")
+    var caller = ev.target || e.srcElement;
+    var elem = $(caller).parent();
+    var text = $(elem).clone().children().remove().end().text();
+    $(elem).empty();
+    var input = $("<input type='text'/>");
+    input.val(text);
+    var ok = $("<button>OK</button>");
+
+    $(ok).click(modifica);
+    elem.append(input);
+    elem.append(ok);
+    $("#tasklist").listview("refresh");
+}
+
+function modifica(ev) {
+    console.log("edita")
+    var caller = ev.target || e.srcElement;
+    var elem = $(caller).parent();
+    var text = $(elem).find("input").val();
+
+    $(elem).empty();
+
+    var delbtn = $("<button style='float:right;'>X</button>");
+    var editbtn = $("<button style='float:right;'>edit</button>");
+    $(delbtn).click(esborra);
+    $(editbtn).click(edita);
+
+    $(elem).append(text);
+    $(elem).append(delbtn);
+    $(elem).append(editbtn);
+    
+    $("#tasklist").listview("refresh");
+} */
